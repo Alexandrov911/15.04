a76f99ffc39e28cc319a4da48b0c60bb50242ef8
Alexandrov911 <sashok17062002@mail.ru>
2021-04-15 11:10:06 +0300

    15.04

---
+++ 15.04/15.04.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._04._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("введите число");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            if (r % 5 == 0) 
+  
+            {
+                Console.WriteLine("четное");
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }
+             Console.ReadKey(); 
+    }
+}
---
+++ 15.04/15.04.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._04._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            if (r % 7 == 0 ||
+                r % 3 == 0)
+
+            {
+                Console.WriteLine("четное");
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
